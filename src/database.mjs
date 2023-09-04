9251a58ec43b852be178966f92924f76d8195a59
Juan Daniel <juandanielarce398@gmail.com>
2023-09-04 15:30:16 -0500

    integration with prettier

--- src/database.mjs
+++ src/database.mjs
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 /**
  * @description Connect to the database
@@ -9,13 +9,13 @@ import mongoose from 'mongoose';
  **/
 export async function connect() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/mongodbgraphql', {
+    await mongoose.connect("mongodb://localhost:27017/mongodbgraphql", {
       useNewUrlParser: true,
     });
 
-    console.log('Database is connected');
+    console.log("Database is connected");
   } catch (error) {
     console.error(error);
-    console.log('Database is not connected');
+    console.log("Database is not connected");
   }
 }
